3f5798c7114f977abaad165d749fd3fad691717a
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-04-24 06:33:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def newchannel(message,chan):
                                   database = "d7tofl99vg7pq2")
     channe = str(chan)
     cursor = connection.cursor()
-    sql_select_query = "SELECT kanal FROM grs"
+    sql_select_query = "SELECT kanal FROM grs WHERE grid=message.chat.id"
     cursor.execute(sql_select_query)
     record = cursor.fetchone()
     msg = str(record)
